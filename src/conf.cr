6114268a7337fba37eb17fde2b6dd08d49735fc3
Fraser McCrossan <fraser@mccrossan.ca>
2020-07-10 20:25:44 -0400

    really fix project name in license headers this time

--- src/conf.cr
+++ src/conf.cr
@@ -1,14 +1,14 @@
 # This file is part of ldif2json.
 
-# gls is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# ldif2json is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
 # later version.
 
-# gls is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# ldif2json is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details.
 
-# You should have received a copy of the GNU General Public License along with gls.  If not, see
+# You should have received a copy of the GNU General Public License along with ldif2json.  If not, see
 # <https://www.gnu.org/licenses/>.
 
 require "option_parser"
